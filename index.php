5bb0f4a26e3ff2cd7714b0b29f83f1244c84d5ed
Rajesh <vijay.reddy@zsspl.in>
2018-06-12 16:17:40 +0530

    index page chages are done

--- index.php
+++ index.php
@@ -182,52 +182,41 @@
 <!-- /vission -->
 
 
-<!-- services1 -->
-<section class="container-fluid sec_services">
+<!-- clients -->
+<section class="container-fluid sec_clients">
 	<article class="container services">
 		<div class="row">
-			<div class="col-md-12">
-				<h4 class="text-center services-h">OUR OFFERINGS</h4>
-				<!-- services1 -->
-				<div class="col-md-4 services_1 rel text-center">
-					<p class=""><img class="offerings_img" src="images\offerings\services_icon_cloud.png"/></p><!-- height="90px" width="90x" -->
-					<p class="services_sub_head text-uppercase">Cloud</p>
-					<p class="hLine col-sm-offset-5 col-sm-2"> </p>
-					<div class="clearfix"></div>
-					<p class="services-p wow slideInDown ">
-						Achieve maximum cost savings and business agility. Ensure that your cloud-based resources are working optimally. We offer cloud computing technical services to our clients to fully realize their cloud initiatives including planning and migrating existing infrastructure and applications to IaaS or SaaS Cloud.
-					</p>
-				</div>
-				<!-- services1 -->
-				<!-- services2 -->
-				<div class="col-md-4 services_2 wow slideInDown text-center">
-					<p class=""><img class="offerings_img" src="images\offerings\services_icon_enterpriceSecurityServices.png"/></p>
-					<p class="services_sub_head text-uppercase">Enterprise Security Services</p>
-					<p class="hLine col-sm-offset-5 col-sm-2"> </p>
-					<div class="clearfix"></div>
-					<p class="services-p">
-						Security is a crucial part of any organization. With our Identity Management services, we help an Organization to manage their Principle with respect to Authentication, Authorization, Privileges for individual system or across organization. Our goal is to provide maximum security with minimal cost and faster deployment.
-					</p>
-				</div>
-				<!-- services2 -->
-				<!-- services3 -->
-				<div class="col-md-4 services_3 wow slideInDown text-center">
-					<p class=""><img class="offerings_img" src="images\offerings\services_icon_businessIntelligence.png"/></p>
-					<p class="services_sub_head text-uppercase">Business Intelligence</p>
-					<p class="hLine col-sm-offset-5 col-sm-2"> </p>
-					<div class="clearfix"></div>
-					<p class="services-p">
-						If your organization is struggling to make key business decisions, our BI practices can track and analyze your data to pinpoint the problem. We utilize only the best industry practices and technologies such as data analytics, business performance management, dashboards and KPIs to pinpoint solutions that can streamline your business.					
-					</p>
-				</div>
-
-
-			</div><!-- col-12 -->
-			
+			<div class="col-md-11"> 
+				<div class="col-md-12">
+					<h2 class="text-center sunSoft-head">OUR CLIENTS</h2>
+					<div class="col-md-12 text-center"><p class="underline"></p></div>
+					<!-- clients1 -->
+					<div class="col-md-3 clients_1">
+						<p class="clients-p wow slideInDown"><img src="images/client.png" class="client_img"  height="140px" width="200px"/></p>
+					</div>
+					<!-- clients1 -->
+					<!-- clients2 -->
+					<div class="col-md-3 clients_2 wow slideInDown">
+						<p class="clients-p"><img src="images/client.png" class="client_img"  height="140px" width="200px"/></p>
+					</div>
+					<!-- clients2 -->
+					<!-- clients3 -->
+					<div class="col-md-3 clients_3 wow slideInDown">
+						<p class="clients-p"><img src="images/client.png" class="client_img"  height="140px" width="200px"/></p>
+					</div>
+					<!-- clients3 -->
+					<!-- clients4 -->
+					<div class="col-md-3 clients_4 wow slideInDown">
+						<p class="clients-p"><img src="images/client.png" class="client_img"  height="140px" width="200px" /></p>
+					</div>
+					<!-- clients4 -->
+				</div><!--col-10-->
+			</div><!-- col-10 -->
 		</div><!-- row -->
 	</article>
 </section>
-<!-- /services1 -->
+<!-- /clients -->
+
 
 <!-- testmonial -->
 <section class="container-fluid sec_testmonial">	
@@ -332,40 +321,6 @@
 </section>
 <!-- /section_details -->
 
-<!-- clients -->
-<section class="container-fluid sec_clients">
-	<article class="container services">
-		<div class="row">
-			<div class="col-md-11"> 
-				<div class="col-md-12">
-					<h2 class="text-center sunSoft-head">OUR CLIENTS</h2>
-					<div class="col-md-12 text-center"><p class="underline"></p></div>
-					<!-- clients1 -->
-					<div class="col-md-3 clients_1">
-						<p class="clients-p wow slideInDown"><img src="images/client.png" class="client_img"  height="140px" width="200px"/></p>
-					</div>
-					<!-- clients1 -->
-					<!-- clients2 -->
-					<div class="col-md-3 clients_2 wow slideInDown">
-						<p class="clients-p"><img src="images/client.png" class="client_img"  height="140px" width="200px"/></p>
-					</div>
-					<!-- clients2 -->
-					<!-- clients3 -->
-					<div class="col-md-3 clients_3 wow slideInDown">
-						<p class="clients-p"><img src="images/client.png" class="client_img"  height="140px" width="200px"/></p>
-					</div>
-					<!-- clients3 -->
-					<!-- clients4 -->
-					<div class="col-md-3 clients_4 wow slideInDown">
-						<p class="clients-p"><img src="images/client.png" class="client_img"  height="140px" width="200px" /></p>
-					</div>
-					<!-- clients4 -->
-				</div><!--col-10-->
-			</div><!-- col-10 -->
-		</div><!-- row -->
-	</article>
-</section>
-<!-- /clients -->
 
 </main> <!--container-fluid -->
 <!-- mainpage.php Ends-->
---
+++ jbin/content.php
@@ -0,0 +1,46 @@
+<!-- services1 -->
+<section class="container-fluid sec_services">
+	<article class="container services">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="text-center services-h">OUR OFFERINGS</h4>
+				<!-- services1 -->
+				<div class="col-md-4 services_1 rel text-center">
+					<p class=""><img class="offerings_img" src="images\offerings\services_icon_cloud.png"/></p><!-- height="90px" width="90x" -->
+					<p class="services_sub_head text-uppercase">Cloud</p>
+					<p class="hLine col-sm-offset-5 col-sm-2"> </p>
+					<div class="clearfix"></div>
+					<p class="services-p wow slideInDown ">
+						Achieve maximum cost savings and business agility. Ensure that your cloud-based resources are working optimally. We offer cloud computing technical services to our clients to fully realize their cloud initiatives including planning and migrating existing infrastructure and applications to IaaS or SaaS Cloud.
+					</p>
+				</div>
+				<!-- services1 -->
+				<!-- services2 -->
+				<div class="col-md-4 services_2 wow slideInDown text-center">
+					<p class=""><img class="offerings_img" src="images\offerings\services_icon_enterpriceSecurityServices.png"/></p>
+					<p class="services_sub_head text-uppercase">Enterprise Security Services</p>
+					<p class="hLine col-sm-offset-5 col-sm-2"> </p>
+					<div class="clearfix"></div>
+					<p class="services-p">
+						Security is a crucial part of any organization. With our Identity Management services, we help an Organization to manage their Principle with respect to Authentication, Authorization, Privileges for individual system or across organization. Our goal is to provide maximum security with minimal cost and faster deployment.
+					</p>
+				</div>
+				<!-- services2 -->
+				<!-- services3 -->
+				<div class="col-md-4 services_3 wow slideInDown text-center">
+					<p class=""><img class="offerings_img" src="images\offerings\services_icon_businessIntelligence.png"/></p>
+					<p class="services_sub_head text-uppercase">Business Intelligence</p>
+					<p class="hLine col-sm-offset-5 col-sm-2"> </p>
+					<div class="clearfix"></div>
+					<p class="services-p">
+						If your organization is struggling to make key business decisions, our BI practices can track and analyze your data to pinpoint the problem. We utilize only the best industry practices and technologies such as data analytics, business performance management, dashboards and KPIs to pinpoint solutions that can streamline your business.					
+					</p>
+				</div>
+
+
+			</div><!-- col-12 -->
+			
+		</div><!-- row -->
+	</article>
+</section>
+<!-- /services1 -->
